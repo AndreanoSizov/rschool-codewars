3330f3c9881c913c43f12ec6d10f6ce425291216
Andrey Sizov <andreano.mpe@gmail.com>
2021-09-29 05:15:10 +0500

    feat: finish task, but not working

--- script.js
+++ script.js
@@ -4,29 +4,57 @@
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
-function capitalization(some_string){
-    const myArray = some_string.split('');
-    const newArray = [];
-    let prevSpace = false;
-    let currentLetter = "";
+function capitalization(some_string) {
+    // const myArray = some_string.split('');   //Blanes on .split() function
+    // const newArray = [];
+    // let prevSpace = false;
+    // let currentLetter = "";
 
-    for (let i=0; i < myArray.length; i++){
-        currentLetter = myArray[i];
+    // for (let i=0; i < myArray.length; i++){
+    //     currentLetter = myArray[i];
+
+    //     if (prevSpace) {
+    //         newArray.push(myArray[i].toUpperCase());
+    //         prevSpace = false;
+    //     } else {
+    //         newArray.push(myArray[i]);
+    //     }
+
+    //     if (myArray[i] == ' ') {
+    //         prevSpace = true;
+    //     }
+
+    //     return newArray.join();
+    // }
+
+    //--- Code Bellow is working, but test throuhs out "TypeError: Cannot read property 'length' of undefined"
+
+    let firstLetter = '';
+    let prevSpace = false;
+    let newString = '';
+
+    for (let i = 0; i < some_string.length; i++) {
 
         if (prevSpace) {
-            newArray.push(myArray[i].toUpperCase());
+            firstLetter = some_string[i].toUpperCase();
+            newString = newString + firstLetter;
             prevSpace = false;
+
         } else {
-            newArray.push(myArray[i]);
+            firstLetter = some_string[i];
+            newString = newString + firstLetter;
         }
 
-        if (myArray[i] == ' ') {
+        if (firstLetter == ' ') {
             prevSpace = true;
-        }
- 
-        return newArray.join();
+        } 
+
+        // console.log(newString);
     }
+
+    console.log(newString);
+    // return newString;
 
 }
 
-capitalization ('How can mirrors be real if our eyes aren\'t real');
+capitalization('How can mirrors be real if our eyes aren\'t real');
